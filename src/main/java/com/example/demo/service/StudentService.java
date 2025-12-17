7045e3cd97fffbac82ab207e1be54f2fc81fb4b8
bala-amypo <bala@amypo.in>
2025-12-17 08:59:03 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,9 +1,14 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.StudentEntity;
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.entity.Student;
 
 public interface StudentService {
 
-    public StudentEntity addStudents(StudentEntity student);
-
+    Student insertStudent(Student st);
+    List<Student> getAllStudents();
+    Optional<Student> getOneStudent(Long id);
+    void deleteStudent(Long id);
 }
--- src/main/java/com/example/demo/service/impl/StudentServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceimpl.java
@@ -1,21 +1,38 @@
 package com.example.demo.service.impl;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.StudentEntity;
-import com.example.demo.repository.StudentRepository;
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepo;
 import com.example.demo.service.StudentService;
 
 @Service
-public class StudentServiceImpl implements StudentService {
+public class StudentServiceimpl implements StudentService {
 
     @Autowired
-    StudentRepository repo;
+    StudentRepo strepo;
 
     @Override
-    public StudentEntity addStudents(StudentEntity student) {
-        return repo.save(student);
+    public Student insertStudent(Student st) {
+        return strepo.save(st);
     }
 
+    @Override
+    public List<Student> getAllStudents() {
+        return strepo.findAll();
+    }
+
+    @Override
+    public Optional<Student> getOneStudent(Long id) {
+        return strepo.findById(id);
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+        strepo.deleteById(id);
+    }
 }
