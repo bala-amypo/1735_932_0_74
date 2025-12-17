f4461ac2e98ed3289e58a28f302130ac097b57cc
bala-amypo <bala@amypo.in>
2025-12-17 08:43:20 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,14 +1,9 @@
 package com.example.demo.service;
 
-import java.util.List;
-import java.util.Optional;
-
-import com.example.demo.entity.Student;
+import com.example.demo.entity.StudentEntity;
 
 public interface StudentService {
 
-    Student insertStudent(Student st);
-    List<Student> getAllStudents();
-    Optional<Student> getOneStudent(Long id);
-    void deleteStudent(Long id);
+    public StudentEntity addStudents(StudentEntity student);
+
 }
--- src/main/java/com/example/demo/service/impl/StudentServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceimpl.java
@@ -1,27 +1,21 @@
 package com.example.demo.service.impl;
-import java.util.List;
-import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.demo.entity.Student;
-import com.example.demo.repository.StudentRepo;
-import com.example.demo.service.StudentService;
-@service
-public class StudentServiceimpl implements StudentService{
-    @Autowired
-    @StudentRepo
-    @override
-    public Student insertStudent(Student st){
-        return  strepo.save(st);
-    }
-    @override
-    public  List<Student> getAllStudents(){
-        return findAll();
-    }
 
-    @override
-    public void deleteStudent(Long id){
-        
+import com.example.demo.entity.StudentEntity;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.service.StudentService;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    StudentRepository repo;
+
+    @Override
+    public StudentEntity addStudents(StudentEntity student) {
+        return repo.save(student);
     }
 
 }
