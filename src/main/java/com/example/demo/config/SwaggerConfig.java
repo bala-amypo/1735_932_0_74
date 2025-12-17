097816e39051f16d8cae16c9974e43dc79a6b9c6
bala-amypo <bala@amypo.in>
2025-12-17 09:56:15 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -11,10 +11,10 @@ public class SwaggerConfig{
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9089.408procr.amypo.ai/")
-                ));
+        return new OpenAPI();
+                // // You need to change the port as per your server
+                // .servers(List.of(
+                //         new Server().url("https://9089.408procr.amypo.ai/")
+                // ));
         }
 }
--- src/main/java/com/example/demo/service/impl/StudentServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceimpl.java
@@ -1,38 +1,39 @@
+
 package com.example.demo.service.impl;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.Student;
-import com.example.demo.repository.StudentRepo;
+import com.example.demo.entity.StudentEntity;
+import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
 
 @Service
-public class StudentServiceimpl implements StudentService {
+public class StudentServiceImpl implements StudentService {
 
     @Autowired
-    StudentRepo strepo;
+    StudentRepository repo;
 
     @Override
-    public Student insertStudent(Student st) {
-        return strepo.save(st);
+    public StudentEntity addStudents(StudentEntity student) {
+        return repo.save(student);
     }
 
     @Override
-    public List<Student> getAllStudents() {
-        return strepo.findAll();
+    public List<StudentEntity> getStudents() {
+        return repo.findAll();
     }
 
     @Override
-    public Optional<Student> getOneStudent(Long id) {
-        return strepo.findById(id);
+    public StudentEntity getStudentById(Long id) {
+        return repo.findById(id).orElse(null);
     }
 
     @Override
-    public void deleteStudent(Long id) {
-        strepo.deleteById(id);
+    public void deleteStudentById(Long id) {
+        repo.deleteById(id);
     }
+
 }
